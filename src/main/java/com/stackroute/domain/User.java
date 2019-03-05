e3528f5b83930868662cd804e791781420e77e40
sharma12344 <pratima447@gmail.com>
2019-03-05 23:42:39 +0530

    first commit

---
+++ src/main/java/com/stackroute/domain/User.java
@@ -0,0 +1,18 @@
+package com.stackroute.domain;
+
+public class User {
+
+
+    private String username;
+
+
+    public String getUsername()
+    {
+        return username;
+    }
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+}
